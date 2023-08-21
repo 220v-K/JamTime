b46cc192cc47c31e4bf730962b03fed6d1045e59
220v-K <jwlee010222@gmail.com>
2023-08-21 23:09:39 +0900

    first commit

---
+++ Sonoma/SonomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonomaApp.swift
+//  Sonoma
+//
+//  Created by 이재원 on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SonomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
